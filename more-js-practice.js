9c9e5c15f6f196ccd04dbac6c94eafe1a22a950b
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-06-08 16:42:25 -0400

    Create more-js-practice.js

---
+++ more-js-practice.js
@@ -0,0 +1,26 @@
+// Problem 1
+// declare a variable called fruit
+// assign your variable the value 6
+// console log your variable
+
+
+// write a function called product that accepts two numbers and 
+// logs the product in the console and then
+// returns the answer when you multiply them both together
+// call that function on the numbers 4 & 689 to test it
+
+// Problem 2
+// write a function called evens that takes an array of numbers, removes 
+// all the numbers that are odd and returns a new array with only the even numbers
+// remember that even numbers have a remainder of 0 when you divide them by 2
+// call your function on this array of numbers [4, 37, 18, 29, 52, 103, 22]
+
+//Problem 3
+// write a function called five that takes a word and prints it in the console
+// five times
+// call your function on the word "pineapple" to test it
+
+//Problem 4
+// write a function called shortWords that takes an array of words and then
+// returns a new array with only the words that are 3 letters long or less
+// call your function on this array ["basin", "pit", "cheese", "bun", "river"]
